eb06e5cbfd265ea0cfa1bbc0740128daad43a58e
Supachai Sakupan <caneliean@gmail.com>
2023-08-02 11:09:11 +0700

    NotLasttimetofix2

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
-import getCurrentUser from "./actions/getCurrentUser";
+import Container from "./components/Container";
+import ListingCard from "./components/listings/ListingCard";
+import EmptyState from "./components/EmptyState";
+import ClientOnly from "./components/clientOnly";
+
 import getListings, {
   IListingsParams,
 } from "./actions/getListings";
-
-import { GetServerSidePropsContext } from "next";
-import { NextPageContext } from "next";
-
-import ClientOnly from "@/app/components/clientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCard";
+import getCurrentUser from "./actions/getCurrentUser";
 
 interface HomeProps {
-  listings: any[];
-  currentUser: any;
+  searchParams: IListingsParams;
 }
 
-const Home = ({ listings, currentUser }: HomeProps) => {
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
+
   if (!listings || listings.length === 0) {
     return (
       <ClientOnly>
@@ -30,48 +29,28 @@ const Home = ({ listings, currentUser }: HomeProps) => {
       <Container>
         <div
           className="
-        mt-10
-        pt-24
-        grid
-        grid-cols-1
-        sm:grid-cols-3
-        md:grid-cols-4
-        xl:grid-cols-5
-        2xl:grid-cols-6
-        gap-8
+          mt-10
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-3
+          md:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
         "
         >
-          {listings.map((listing) => {
-            return (
-              <ListingCard
-                key={listing.id}
-                data={listing}
-                currentUser={currentUser}
-              />
-            );
-          })}
+          {listings.map((listing: any) => (
+            <ListingCard
+              key={listing.id}
+              data={listing}
+              currentUser={currentUser}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   );
 };
-
-export async function getServerSideProps(
-  context: GetServerSidePropsContext
-) {
-  if (!context.params) {
-    // Return some default props in case params is undefined
-    return { props: {} };
-  }
-
-  const userId = Array.isArray(context.params.userId)
-    ? context.params.userId[0]
-    : context.params.userId; // This gets the userId from the route
-
-  const listings = await getListings({ userId });
-  const currentUser = await getCurrentUser();
-
-  return { props: { listings, currentUser } };
-}
 
 export default Home;
